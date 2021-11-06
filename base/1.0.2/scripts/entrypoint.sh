dcfbeed73a851a25e5afd48b549748dfa079057c
Ioannis Dritsas <dream.paths.projekt@gmail.com>
2021-11-06 16:01:20 +0200

    Fixes piopi/docker-desktop#8 (#9)

--- base/1.0.2/scripts/entrypoint.sh
+++ base/1.0.2/scripts/entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 trap ctrl_c INT
 function ctrl_c() {
   exit 0
-} 
+}
 # entrypoint.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server.
 rm /tmp/.X1-lock 2> /dev/null &
 /opt/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
-vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None &
+# Insecure option is needed to accept connections from the docker host.
+vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
 wait
